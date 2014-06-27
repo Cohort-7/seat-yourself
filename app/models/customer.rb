885e442dc70ac954d835d69c65473a01a325fe52
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-27 15:04:40 -0400

    model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,8 @@ class Customer < ActiveRecord::Base
 	has_many :reviews
 	has_many :reservations
 	has_many :restaurants, :through => :reviews
+	# has_many :owned_restaurants, => :class_name => "Restaurant", :foreign_key => "owner_id"
+
+	validates :name, :email, :presence => true
+	validates :email, :uniqueness => true
 end
