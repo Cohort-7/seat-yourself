1ffa2ade43024bba94ef67c918fc2dbc643842ed
brandoshmando <brancraft@gmail.com>
2014-06-28 13:53:22 -0400

    Commented out restaurant id setter in reservations controller because it broke it.... That'll have to be fixed somehow

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,7 +7,7 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(parse_new_time_object(reservation_params))
-    @reservation.restaurant_id = @restaurant.id
+    #@reservation.restaurant_id = @restaurant.id
     # @reservation.customer_id = current_customer.id
 
     if @reservation.save
