26efd3e185936ceb47c523b70e72271d4876c458
brandoshmando <brancraft@gmail.com>
2014-06-30 15:37:19 -0400

    Added some stuff. Worst commit message ever

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,13 +2,22 @@ class Reservation < ActiveRecord::Base
 	belongs_to :customer
 	belongs_to :restaurant
 
-  validates :reservation_availabiliy
+  validate :reservation_availability
 
   def reservation_availability
-    restuarant.reservations.each do |current_reservation|
-      if reservation.time == current_reservation.time
-        errors.add(reservation: "time is not available...")
+    restaurant.reservations.each do |current_reservation|
+      if time == current_reservation.time && restaurant.capacity < hourly_capacity(restaurant) + people
+        puts "WORKED"
+        errors.add(:reservation, "time is not available...")
       end
+    end
+  end
+
+  def hourly_capacity(restaurant)
+    capacity = []
+    reservervations = restaurant
+    restaurant.reviews.each {|review| capacity << review.people }
+    capacity.inject(0) {|sum, num| sum + num}
     end
   end
 end
