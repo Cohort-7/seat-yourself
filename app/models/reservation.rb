526ea52c22a27e1231089eb02081d274deea399f
brandoshmando <brancraft@gmail.com>
2014-07-02 22:47:14 -0400

    Reservation validation is up and running just beautifully

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,22 +1,19 @@
 class Reservation < ActiveRecord::Base
-	belongs_to :customer
-	belongs_to :restaurant
+  belongs_to :customer
+  belongs_to :restaurant
 
-  # validate :reservation_availability
+  validate :reservation_availability
 
   def reservation_availability
-    restaurant.reservations.each do |current_reservation|
-      if time == current_reservation.time && restaurant.capacity < hourly_capacity(restaurant) + people
-        puts "WORKED"
-        errors.add(:reservation, "time is not available...")
-      end
+    if restaurant.capacity < hourly_capacity(time, restaurant_id) + people
+      errors.add(:reservation, "time is not available...")
     end
   end
 
-  def hourly_capacity(restaurant)
-    capacity = []
-    reservations = restaurant
-    restaurant.reviews.each {|review| capacity << review.people }
-    capacity.inject(0) { |sum, num| sum + num }
+  def hourly_capacity(time, restaurant_id)
+    people = []
+    reservations = Reservation.where("time = ? AND restaurant_id = ?", time, restaurant_id)
+    reservations.each {|reservation| people << reservation.people }
+    people.inject(0) {|sum, num| sum + num }
   end
 end
